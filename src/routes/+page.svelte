e3e38d26962506b00a1f42223bedcf96032e54ef
Rajeev Rajchal <rajeev@setsnail.com>
2024-09-19 21:46:27 +0200

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div class="background" style:--background="red">
+	<p>the games hello world</p>
+</div>
+
+<style>
+	.background { 
+		width: 100vw;
+		height: 100vh;
+		background: var(--background);
+	}
+</style>
